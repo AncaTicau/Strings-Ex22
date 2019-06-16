855f6db61c252ea2c01b5e9cba041bd6b624147b
MariusA <anca.ticau@gmail.com>
2019-06-17 01:00:11 +0300

    Strings-Ex22

---
+++ Strings-Ex22/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "ala bala portocala";
+            string occurence = "ala";
+            Console.WriteLine(CountOccurrencesOfASubstring(givenString, occurence));
+        }
+        public static int CountOccurrencesOfASubstring(string input, string findOccurence)
+        {
+            int index = 0;
+            int count = 0;
+            int length = input[index];
+            while ((index = input.IndexOf(findOccurence, index)) >= 0)
+            {
+                index += length; count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Strings-Ex22/Strings-Ex22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace Strings_Ex22
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "This is a string and it is awesome.";
+            Console.WriteLine(CapitalizeFirstAndLastLetters(input));
+        }
+
+        //Write a method to capitalize first and last letters of each word of a given string.
+        public static string CapitalizeFirstAndLastLetters(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (!char.IsLetter(input[i]))
+                {
+                    result.Append(input[i]);
+                    continue;
+                }
+
+                char currentChar = input[i];
+
+                if (i == 0 || !char.IsLetter(input[i - 1]))
+                {
+                    result.Append(char.ToUpper(currentChar));
+                    continue;
+                }
+
+                if (i == input.Length - 1 || !char.IsLetter(input[i + 1]))
+                {
+                    result.Append(char.ToUpper(currentChar));
+                    continue;
+                }
+
+                result.Append(input[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
+
+
